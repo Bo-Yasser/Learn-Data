bea4c734ef3c0f2a2c6f136b94defe59988415f7
Bo-Yasser <mohamedyasser1960@gmail.com>
2024-12-30 22:35:58 +0200

    update Stack folder

--- Python/DataStructure/Stack/linked_stack.py
+++ Python/DataStructure/Stack/linked_stack.py
@@ -5,26 +5,44 @@ class Node:
 
 class LinkedStack:
     def __init__(self):
-        self.head = None
+        self.__head = None
         self.__top = -1
 
+    def __str__(self):
+        return f"Stack({list(self)})"
+    
+    def __iter__(self):
 
+        current = self.__head
+        while current:
+            yield current.value
+            current = current.next
+    
+    def __contains__(self, value):
+        return self.is_found(value)
+    
     def search(self, element):
-        current = self.head
+        if self.is_empty():
+            raise IndexError("Stack is empty.")
+        
+        current = self.__head
         count = self.__top 
         while current:
             if current.value == element:
                 return count
             current = current.next
             count -= 1
-        return -1
+        return f"{element} not found."
     
 
     def peek(self, index):
+        if self.is_empty():
+            raise IndexError("Stack is empty.")
+        
         if index > self.__top or index < 0:
             raise IndexError("Index Out Of Range.")
         
-        current = self.head
+        current = self.__head
         count = self.__top 
         while current:
             if count == index:
@@ -32,14 +50,14 @@ class LinkedStack:
             current = current.next
             count -= 1
 
-        raise IndexError("Index Out of Range.")
+        raise IndexError("element not found.")
     
     
     def push(self, value):
         new_node = Node(value)
         
-        new_node.next = self.head
-        self.head = new_node
+        new_node.next = self.__head
+        self.__head = new_node
         self.__top += 1
         
     def pop(self, value=None):
@@ -47,13 +65,13 @@ class LinkedStack:
             raise IndexError("Stack Is Empty. Nothing to Pop.")
 
 
-        if value is None or self.head.value == value:
-            popped_value = self.head.value
-            self.head = self.head.next
+        if value is None or self.__head.value == value:
+            popped_value = self.__head.value
+            self.__head = self.__head.next
             self.__top -= 1
             return popped_value
 
-        current = self.head
+        current = self.__head
         while current.next:
             if current.next.value == value:
                 popped_value = current.next.value
@@ -62,73 +80,63 @@ class LinkedStack:
                 return popped_value
             current = current.next
 
-        raise ValueError(f"Value {value} not found in the stack.")
+        raise IndexError(f"Value {value} not found in the stack.")
 
     def sort(self, reverse=False):
-        values = []
-        while not self.is_empty():
-            values.append(self.pop())
-            values.sort(reverse=reverse)
-        
-        for val in values:
-            self.push(val)
+        values = list(self)
+        values.sort(reverse=reverse)
+        self.clear()
+        for value in reversed(values):
+            self.push(value)
     
     def is_sorted(self):
-        current = self.head
+        current = self.__head
         while current and current.next:
-            if current.value < current.next.value:
+            if current.value > current.next.value:
                 return False
             current = current.next
         return True
-    
+
     def merge(self, other_stack):
-        temp = []
-        current = other_stack.head
-        while current:
-            temp.append(current.value)
-            current = current.next
-
-
-        for value in reversed(temp):
+        if not isinstance(other_stack, LinkedStack):
+            raise TypeError("must merge stack with another stack.")
+        for value in reversed(list(other_stack)):
             self.push(value)
 
     def reverse(self):
-        current = self.head
+        current = self.__head
         prev = None
         while current:
             next_node = current.next
             current.next = prev
             prev = current
             current = next_node
-        self.head = prev
+        self.__head = prev
     
     def display(self):
-        current = self.head
-        elements = []
-        if not current:
-            print("Stack is Empty.")
-            return
-        
-        while current:
-            elements.append(str(current.value))
-            current = current.next
-        print(" -> ".join(elements), end=" -> ")
-        print("None")
-        
-        
+        return list(self)
     
     def clear(self):
-        self.head = None
+        self.__head = None
+        self.__top = -1
     
     def is_empty(self):
-        return self.head == None
+        return self.__head == None
+    
+    def is_found(self, element):
+        current = self.__head
+        while current:
+            if current.value == element:
+                return True
+            current = current.next
+        return False    
     
     def count(self):
         return self.__top + 1
-    
+
     def top(self):
         if not self.is_empty():
-            return self.head.value
+            return self.__head.value
     
     def size(self):
         return self.__top + 1
@@ -145,14 +153,14 @@ if __name__ == "__main__":
     stack.push(30)
     stack.pop(20)
     print(stack.top())
-    stack.display()
+    print(stack.display())
 
     print("#"* 50)
     stack.pop()
     stack.push(30)
     stack.push(40)
     print(stack.top())
-    stack.display()
+    print(stack.display())
 
     print("#"* 50)
     print(stack.peek(0))
@@ -160,7 +168,7 @@ if __name__ == "__main__":
 
     print("#"* 50)
     stack.reverse()
-    stack.display()
+    print(stack.display())
     print(stack.top())
 
     print("#"* 50)
@@ -169,18 +177,25 @@ if __name__ == "__main__":
 
     stack.sort()
     print(stack.is_sorted())
-    stack.display()
+    print(stack.display())
 
-    print("#"* 50)
-    print("#"* 50)
+    # print("#"* 50)
+    # print("#"* 50)
 
-    stack2.push(400)
-    stack2.push(300)
-    stack2.display()
-    print(stack.top())
+    # stack2.push(400)
+    # stack2.push(300)
+    # print(stack2.display())
+    # print(stack.top())
 
-    stack.merge(stack2)
-    stack.display()
-    print(stack.top())
+    # stack.merge(stack2)
+    # print(stack.display())
+    # print(stack.top())
 
-
+    # print("#"* 50)
+    # print("#"* 50)
+    
+    # print(stack)
+    # for i in stack:
+    #     print(i, end='-')
+    # if 10 in stack:
+    #     print("\na4ta")
--- Python/DataStructure/Stack/stack.py
+++ Python/DataStructure/Stack/stack.py
@@ -10,8 +10,18 @@ class Stack:
         self.__default_value = self.__arr[0]
         self.__top = -1
     
+    # return readable str when print the object
     def __str__(self):
         return f"Stack({self.display()})"
+    
+    # support iteration for any stack object
+    def __iter__(self):
+        reversed_stack = self.__arr[:self.__top + 1][::-1]
+        return iter(reversed_stack)
+    
+    # support in condition
+    def __contains__(self, value):
+        return self.is_found(value)
     
     # Push Elements
     def push(self, data):
@@ -57,6 +67,7 @@ class Stack:
         
         raise IndexError("Stack Is Empty on top")
     
+    # merge with another stack
     def merge(self, other_stack):
         if not isinstance(other_stack, Stack):
             raise TypeError("Can only merge with another Stack.")
@@ -77,26 +88,43 @@ class Stack:
             raise IndexError("Index out of range")
         return self.__arr[index]
     
+    # reverse stack
+    def reverse(self):
+        self.__arr[:self.__top + 1] = self.__arr[:self.__top + 1][::-1]
+
+    # sort stack 
+    def sort(self, reverse=False):
+        self.__arr[:self.__top + 1] = sorted(self.__arr[:self.__top + 1], reverse=reverse)
+
+    def is_sorted(self):
+        for i in range(self.__top + 1):
+            if self.__arr[i] > self.__arr[i + 1]:
+                return False
+        return True 
+
+    # Show If stack is empty
+    def is_empty(self):
+        return self.__top < 0
+    
+    # Show If stack is full
+    def is_full(self):
+        return self.__top + 1 == self.__size
+    
     # Return index for specific element
     def search(self, element):
         for i in range(self.__top + 1):
             if self.__arr[i] == element:
                 return i
         return -1
-
-    def reverse(self):
-        self.__arr[:self.__top + 1] = self.__arr[:self.__top + 1][::-1]
-
-    def sort(self, reverse=False):
-        self.__arr[:self.__top + 1] = sorted(self.__arr[:self.__top + 1], reverse=reverse)
-
-    # Show If Empty
-    def is_empty(self):
-        return self.__top < 0
     
-    def is_full(self):
-        return self.__top + 1 == self.__size
+    # Return True if found and False if not found
+    def is_found(self, element):
+        for i in range(self.__top + 1):
+            if self.__arr[i] == element:
+                return True
+        return False
     
+    # clear stack
     def clear(self):
         self.__arr[:self.__top + 1] = self.__default_value
         self.__top = -1
@@ -108,7 +136,8 @@ class Stack:
     # Show Size 
     def size(self):
         return self.__size
-
+    
+    # return length for stack
     def count(self):
         return self.__top + 1
         
@@ -123,3 +152,9 @@ if __name__ == "__main__":
     stack.push(3)
 
     print(stack.peek(1))
+    print(stack.display())
+    print(stack)
+    stack.sort()
+    print(stack.display())
+    print(stack.top())
+    print(stack.is_sorted())
